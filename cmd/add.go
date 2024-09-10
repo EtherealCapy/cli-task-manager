efdb6e09d5f873ba974a297cacf413f4d48a2b23
Lykan <erz_18@outlook.com>
2024-09-10 22:33:52 +0200

    docs: logic changes

--- cmd/add.go
+++ cmd/add.go
@@ -16,27 +16,33 @@ var addCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 
 		var tarea tareas.Tarea
+
+		if args[0] == "" || args[0] == " " {
+			fmt.Println(" [!] Ingrese un título para la tarea")
+			return
+		}
+
 		tarea.Titulo = args[0]
 		tarea.Completado = false
 		tarea.Fecha = time.Now().Format("Mon, 02 Jan 2006")
 		prioridad, errConv := strconv.Atoi(args[1])
 
 		if errConv != nil {
-			fmt.Println("Error parámetro de prioridad incorrecto")
+			fmt.Println(" [!] Error parámetro de prioridad incorrecto")
 			return
 		}
 
 		if prioridad < 1 || prioridad > 3 {
-			fmt.Println("Rango de prioridad incorrecto")
+			fmt.Println(" [!] Rango de prioridad incorrecto")
 			return
 		}
 
 		tarea.Prioridad = prioridad
 
 		if err := tareas.AddTarea(tarea); err != nil {
-			fmt.Println("Error al añadir la tarea: ", err)
+			fmt.Println("[!] Error al añadir la tarea: ", err)
 		} else {
-			fmt.Println(tarea.Titulo + " añadida a la lista de tareas")
+			fmt.Println(" [*] " + tarea.Titulo + " añadida a la lista de tareas")
 		}
 	},
 }
