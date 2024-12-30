08d590d8dee9aba2dfe86d485bb88b570f6fd971
Emilio <erz_18@outlook.com>
2024-12-30 11:03:08 +0000

    fix: Arreglo en lógica de actualización de dias activos

--- internal/updateActiveDays.go
+++ internal/updateActiveDays.go
@@ -3,7 +3,6 @@ package internal
 import (
 	"fmt"
 	"math"
-	"strconv"
 	"tareas-cli/pkg"
 	"tareas-cli/pkg/models"
 	"time"
@@ -16,7 +15,7 @@ var updateActiveCmd = &cobra.Command{
 	Short: "Update task active days",
 	Long:  "Update the active days for a task",
 	Run: func(cmd *cobra.Command, args []string) {
-		updateActiveDays(args)
+		updateActiveDays()
 	},
 }
 
@@ -24,39 +23,33 @@ func init() {
 	rootCmd.AddCommand(updateActiveCmd)
 }
 
-func updateActiveDays(args []string) {
+func updateActiveDays() {
 
-	if args[0] == "" || args[0] == " " {
-		fmt.Println(" [!] Task ID is required")
-		return
-	}
-
-	id, errConv := strconv.Atoi(args[0])
-
-	if errConv != nil {
-		fmt.Println(" [!] Wrong ID format. Must be a number")
-		return
-	}
-
-	id--
-
-	if id < 0 {
-		fmt.Println(" [!] Invalid ID: out of range")
-		return
-	}
-
-	task, err := pkg.GetTarea(id)
+	tasks, err := pkg.ListTareas()
 
 	if err != nil {
-		fmt.Println(" [!] Task not found")
+		fmt.Println("Error while loading tasks")
 		return
 	}
 
-	activeDays := calculateActiveDays(task)
+	if len(tasks) != 0 {
 
-	if errUpdate := pkg.UpdateActiveDays(id, activeDays); errUpdate != nil {
-		fmt.Println(" [!] Error while updating active days")
-		return
+		for i := range tasks {
+
+			task, err := pkg.GetTarea(i)
+
+			if err != nil {
+				fmt.Println(" [!] Task not found")
+				return
+			}
+
+			activeDays := calculateActiveDays(task)
+
+			if errUpdate := pkg.UpdateActiveDays(i, activeDays); errUpdate != nil {
+				fmt.Println(" [!] Error while updating active days")
+				return
+			}
+		}
 	}
 }
 
